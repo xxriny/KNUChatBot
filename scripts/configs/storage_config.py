cf36794f641868f4a9239076484762c416e84f04
YOOJIIN <yoojiin0328@naver.com>
2025-09-07 02:56:38 +0900

    refactor: storage_config와 blob_utils 함수 재사용성 개선

--- scripts/configs/storage_config.py
+++ scripts/configs/storage_config.py
@@ -7,13 +7,11 @@ load_dotenv()
 class BlobStorageConfig:
     account_url: str
     container: str
-    blob_name: str
     credential: str  # account key or SAS token
 
 def get_blob_config() -> BlobStorageConfig:
     return BlobStorageConfig(
         account_url=os.getenv("AZURE_BLOB_ACCOUNT_URL"),     
         container="data",
-        blob_name="kangwon_notices.csv",
         credential=os.getenv("AZURE_BLOB_CREDENTIAL"),        
     )
--- scripts/utils/blob_utils.py
+++ scripts/utils/blob_utils.py
@@ -3,16 +3,12 @@ import pandas as pd
 from azure.storage.blob import BlobClient
 from scripts.configs.storage_config import get_blob_config
 
-def load_notices_df_from_blob(encoding: str = "utf-8") -> pd.DataFrame:
+def load_notices_df_from_blob(blob_name:str, encoding: str = "utf-8") -> pd.DataFrame:
     cfg = get_blob_config()
-    print(cfg.account_url)
-    print(cfg.blob_name)
-    print(cfg.container)
-    print(cfg.credential)
     blob = BlobClient(
         account_url=cfg.account_url,
         container_name=cfg.container,
-        blob_name=cfg.blob_name,
+        blob_name=blob_name,
         credential=cfg.credential
     )
     data = blob.download_blob().readall()
